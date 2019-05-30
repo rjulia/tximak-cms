449be35f2a7014046445c205b40f3ddfbadb9551
Ramon Julia <reimon1972@gmail.com>
2019-05-30 16:39:17 +0800

    delete all

---
+++ src/pages/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+//import Helmet from "react-helmet";
+
+
+import Layout from "../components/Layout";
+
+import "../components/all.sass";
+
+export const HomePageTemplate = ({ home }) => {
+  console.log(home)
+  return (
+    <p>{home.title}</p>
+  );
+};
+
+class HomePage extends React.Component {
+  render() {
+    const { data } = this.props;
+    console.log(this.props)
+    const {
+      data: { headerData },
+    } = this.props;
+    const { frontmatter: home } = data.homePageData.edges[0].node;
+
+    return (
+      <Layout headerData={headerData}>
+
+        <HomePageTemplate home={home} />
+      </Layout>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+};
+
+export default HomePage;
+
+export const pageQuery = graphql`
+  query HomePageQuery {
+    ...LayoutFragment
+    homePageData: allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "home-page" } } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
