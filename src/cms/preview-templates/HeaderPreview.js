eb01a3c14cef4ded80ea203e4300e8c316ed6d02
Ramon Julia <reimon1972@gmail.com>
2019-06-01 12:47:11 +0800

    fix bud deploy Header

--- src/cms/preview-templates/HeaderPreview.js
+++ src/cms/preview-templates/HeaderPreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { HeaderTemplate } from '../../components/Home/Header/'
+import { HeaderTemplate } from '../../components/Home/index'
 
 const HeaderPreview = ({ entry }) => {
   const data = entry.getIn(['data']).toJS();
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,6 @@ export { default as Carousel } from './Carousel'
 export { default as Location } from './Location'
 export { default as Services } from './Services'
 export { default as Team } from './Team'
-export { Header } from './Header'
+export { Header, HeaderTemplate } from './Header'
 
 
