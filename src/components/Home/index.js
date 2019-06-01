debc0696febc84cb5a66fd063496b7e141fc4e55
Ramon Julia <reimon1972@gmail.com>
2019-06-01 13:09:30 +0800

    fix bugs in compoentes deploy

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 export { default as Carousel } from './Carousel'
-export { default as Location } from './Location'
+export { default as Location } from './Location/Location'
 export { default as Services } from './Services'
 export { default as Team } from './Team'
 export { Header, HeaderTemplate } from './Header'
