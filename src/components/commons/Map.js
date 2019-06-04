049455bd043baa0798eced9c0f5f562eff62693a
Ramon Julia <reimon1972@gmail.com>
2019-06-04 10:50:34 +0800

    fix bug

--- src/components/commons/Map.js
+++ src/components/commons/Map.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-import LocationIcon from "../../img/Location.svg";
+import LocationIcon from "../../img/location.svg";
 
 class Map extends Component {
   render() {
